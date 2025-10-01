3c3f67a3ccff913b9d252f62efcfd2227499c0e4
Andriy06 <andriyhavryliv06@gmail.com>
2025-10-01 23:04:34 +0300

    Update requirements with exact versions (Flask 2.2.5 + Swagger)

--- app/my_project/__init__.py
+++ app/my_project/__init__.py
@@ -1,13 +1,29 @@
 from flask import Flask
-from my_project.database import db
-from my_project.base.routes.__init__ import register_routes
 from config.config import Config
+from my_project.database import db
+from my_project.base.routes import register_routes
 
+
+from flasgger import Swagger
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+
     db.init_app(app)
     register_routes(app)
+
+    # Swagger
+    swagger_template = {
+        "openapi": "3.0.3",
+        "info": {
+            "title": "Concert Bus API",
+            "version": "1.0.0",
+            "description": "Документація до API курсового проєкту"
+        },
+        "servers": [{"url": "/"}]
+    }
+
+    Swagger(app, template=swagger_template)
 
     return app
